2dcf2e4cc49d5f77f7893d20e8794d3e82e9ae48
dominikpajkossy <dominik.pajkossy@gmail.com>
2024-07-01 12:20:51 +0200

    init

---
+++ arcade/databases/soccerPlayers/solution.sql
@@ -0,0 +1,5 @@
+CREATE PROCEDURE solution()
+BEGIN
+	SELECT GROUP_CONCAT(CONCAT_WS('', first_name, ' ', surname, ' #', NULL, player_number) ORDER BY player_number ASC SEPARATOR ', ') as players
+	FROM soccer_team;
+END
